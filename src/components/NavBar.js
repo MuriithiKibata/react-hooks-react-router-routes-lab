5ee69da76852478f6a08a9a12e3cf19074277cf5
Muriithi <muriithi.kibata@student.moringaschool.com>
2022-09-29 19:31:06 +0300

    Done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,21 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return <div className="navbar">
+<NavLink 
+to = "/Home"
+> Home</NavLink> 
+
+<NavLink  to = "/Movies"
+>Movies
+</NavLink>
+<NavLink to = "/Actors">
+Actors
+</NavLink>
+<NavLink to = "/Directors">
+Directors
+</NavLink>
+  </div>;
 }
 
 export default NavBar;
